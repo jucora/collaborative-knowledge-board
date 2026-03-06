be97d93255018ce83fea56b1044bc98f26e0e823
Julian Belmonte <artjulius@hotmail.com>
2026-03-05 20:12:15 -0500

    Implement board member domain repositories

---
+++ lib/features/board_member/domain/repositories/board_member_repository.dart
@@ -0,0 +1,19 @@
+import 'package:collaborative_knowledge_board/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+
+import '../entities/board_member.dart';
+
+abstract class BoardMemberRepository {
+  Future<Either<Failure, List<BoardMember>>> getBoardMembers(String boardId);
+  Future<void> addMemberToBoard({
+    required userId,
+    required String boardId,
+    required String role,
+    required DateTime joinedAt,
+  });
+
+  Future<void> removeMemberFromBoard({
+    required String boardId,
+    required String userId,
+  });
+}
